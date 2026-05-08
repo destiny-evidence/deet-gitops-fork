5f832fb64089571614c5e0064d626c3d44fe74c6
nikloynes <niklasloynes@gmail.com>
2026-05-08 13:43:43 +0100

    fix for `document.document` `AttributeError`

--- deet/data_models/documents.py
+++ deet/data_models/documents.py
@@ -85,7 +85,6 @@ class DocumentIdentity(BaseModel):
 
     document_id: int | None = None
     document_id_source: DocumentIDSource | None = None
-    # external_id: str | int | None = None
     external_id: str | int | None = None
 
     # parsed citation info
